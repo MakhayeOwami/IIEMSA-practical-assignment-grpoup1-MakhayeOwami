2caf94b990d06e64359f6c4c46d827348e516fd6
MakhayeOwami <ST10470040@imconnect.edu.za>
2025-09-04 23:44:47 +0200

    Initial Commit
    1st Commit

---
+++ src/seriesmodel/SeriesModel.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package seriesmodel;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class SeriesModel {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        SeriesManager manager = new SeriesManager();
+        manager.showMenu();
+        
+    }
+    
+}
